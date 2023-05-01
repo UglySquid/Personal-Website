e5ea330b7e4991688f122e8026f0312fbf5235d9
UglySquid <christine.wei.63@gmail.com>
2023-05-01 17:22:53 -0400

    hehehehaw

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,88 +1,98 @@
 import {
     Container, SimpleGrid, Flex, Stack,
-    Img, Skeleton,
+    Img,
     Text, Heading,
-    extendTheme, ChakraProvider, 
-  } from '@chakra-ui/react';
+    extendTheme, ChakraProvider, chakra, shouldForwardProp
+} from '@chakra-ui/react';
 
-  import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import React, { ReactElement } from 'react';
+import { isValidMotionProp, motion } from 'framer-motion';
 
-  import "@fontsource/roboto-mono/500.css"
-  
-  interface FeatureProps {
-    text: string;
-    iconBg: string;
-    icon?: ReactElement;
-  }
+import "@fontsource/roboto-mono/500.css"
 
-  const Font = extendTheme({
-    fonts: {
-      Heading: `'Roboto Mono', monospace;`,
-    },
-  })
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
 
+const Font = extendTheme({
+  fonts: {
+    Heading: `'Roboto Mono', monospace;`,
+  },
+})
 
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+  return (
+    <Stack direction={'row'} align={'center'}>
+      <Flex
+        w={8}
+        h={8}
+        align={'center'}
+        justify={'center'}
+        rounded={'full'}
+        bg={iconBg}>
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 
-  const Feature = ({ text, icon, iconBg }: FeatureProps) => {
-    return (
-      <Stack direction={'row'} align={'center'}>
-        <Flex
-          w={8}
-          h={8}
-          align={'center'}
-          justify={'center'}
-          rounded={'full'}
-          bg={iconBg}>
-          {icon}
+const ChakraBox = chakra(motion.div, {
+  /**
+   * Allow motion props and non-Chakra props to be forwarded.
+   */
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
+export default function Hero() {
+  return (
+    <Container maxW={'5xl'} py={12} centerContent>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} alignItems={'center'}>
+        <Stack spacing={4}>
+          <ChakraProvider theme={Font}>
+            <Heading
+              bgGradient='linear(to-r, blue.200, yellow.200, pink.200)'
+              bgClip='text'
+              fontSize='7xl'
+              fontWeight='extrabold'
+            >
+              Hello There...
+              
+            </Heading>
+          </ChakraProvider>
+          <Text color={'white.500'} fontSize={'lg'}>
+          I&apos;m Christine! I&apos;m currently a grade 10 student at Milliken Mills High School. 
+          In my freetime, I like to read and code. I dunno man I&apos;m kinda basic 😳
+          </Text>
+          
+        </Stack>
+        <Flex justifyContent={'center'}>
+          <ChakraBox  
+            whileHover={{ scale: 1.1}}
+            onHoverStart={e => {}}
+            onHoverEnd={e => {}}
+          >
+            <Img
+              rounded={'md'}
+              alt={'Currently Nonexistent image'}
+              src={
+                'deca.jpg'
+              }
+              objectFit={'cover'}
+              height={250}
+              filter='auto' brightness='70%'
+              // fallbackSrc='https://via.placeholder.com/450x300'
+            />
+          </ChakraBox>
+
+          {/* <canvas class="webgl">
+            <script type='module' src='/cat.js'></script>
+          </canvas> */}
+          {/* 3D Model credit: "ordak the duck" (https://skfb.ly/oEn8s) by ZIRODESIGN is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). */}
         </Flex>
-        <Text fontWeight={600}>{text}</Text>
-      </Stack>
-    );
-  };
-  
-  export default function Hero() {
-    return (
-      <Container maxW={'5xl'} py={12} centerContent>
-        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} alignItems={'center'}>
-          <Stack spacing={4}>
-            <ChakraProvider theme={Font}>
-              <Heading
-                bgGradient='linear(to-r, blue.200, yellow.200, pink.200)'
-                bgClip='text'
-                fontSize='7xl'
-                fontWeight='extrabold'
-              >
-                Hello There...
-                
-              </Heading>
-            </ChakraProvider>
-            <Text color={'white.500'} fontSize={'lg'}>
-            I&apos;m Christine! I&apos;m currently a grade 10 student at Milliken Mills High School. 
-            In my freetime, I like to read and code. I dunno man I&apos;m kinda basic 😳
-            </Text>
-            
-          </Stack>
-          <Flex justifyContent={'center'}>
-            <Skeleton isLoaded>
-              <Img
-                rounded={'md'}
-                alt={'Currently Nonexistent image'}
-                src={
-                  'deca.jpg'
-                }
-                objectFit={'cover'}
-                height={250}
-                // fallbackSrc='https://via.placeholder.com/450x300'
-              />
-            </Skeleton>
-
-            {/* <canvas class="webgl">
-              <script type='module' src='/cat.js'></script>
-            </canvas> */}
-            {/* 3D Model credit: "ordak the duck" (https://skfb.ly/oEn8s) by ZIRODESIGN is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). */}
-          </Flex>
-        </SimpleGrid>
-      </Container>
-    );
-  }
-  
+      </SimpleGrid>
+    </Container>
+  );
+}
--- components/works.tsx
+++ components/works.tsx
@@ -1,149 +1,141 @@
 import {
-    Box,
-    Button,
-    Container,
-    Flex,
-    Heading,
-    Icon,
-    Link,
-    Stack,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  import { ReactElement } from 'react';
+    Container, Stack, Flex, Box,
+    Heading, Text,
+    Icon, Button, Link,
+    useColorModeValue, chakra, shouldForwardProp, 
+} from '@chakra-ui/react';
+import { ReactElement } from 'react';
 
-  import { isValidMotionProp, motion } from 'framer-motion';
-  import React, { useRef, useEffect } from 'react';
-  import { chakra, shouldForwardProp } from '@chakra-ui/react'
+import { isValidMotionProp, motion } from 'framer-motion';
+import React, { useRef, useEffect } from 'react';
 
-  import { BsGithub } from 'react-icons/bs'
-  import { FaFolder } from 'react-icons/fa'
-  import { MdOutlineWebAsset } from 'react-icons/md'
-  
-  interface CardProps {
-    heading: string;
-    description: string;
-    icon: ReactElement;
-    link: ReactElement;
-  }
+import { BsGithub } from 'react-icons/bs'
+import { FaFolder } from 'react-icons/fa'
+import { MdOutlineWebAsset } from 'react-icons/md'
 
-  const ChakraBox = chakra(motion.div, {
-    /**
-     * Allow motion props and non-Chakra props to be forwarded.
-     */
-    shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
-  });
+interface CardProps {
+  heading: string;
+  description: string;
+  icon: ReactElement;
+  link: ReactElement;
+}
 
-  const container = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 1
-      }
+const ChakraBox = chakra(motion.div, {
+  /**
+   * Allow motion props and non-Chakra props to be forwarded.
+   */
+  shouldForwardProp: (prop) => isValidMotionProp(prop) || shouldForwardProp(prop),
+});
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1
     }
   }
-  
-  const item = {
-    hidden: { opacity: 0, scale: 0.5 },
-    show: { opacity: 1, scale: 1 }
-  }
-  
-  
-  const Card = ({ heading, description, icon, link }: CardProps) => {
-    return (
-        <ChakraBox
-          variants={item}
-          initial={{ opacity: 0, rotateX: -10, scale: 0 }}
-          whileInView={{ opacity: 1, rotateX: 0, scale: 1 }}
-          viewport={{ once: true }}
+}
 
-          whileHover={{ scale: 1.1 }}
-          onHoverStart={e => {}}
-          onHoverEnd={e => {}}
+const item = {
+  hidden: { opacity: 0, scale: 0.5 },
+  show: { opacity: 1, scale: 1 }
+}
 
-          maxW={{ base: 'full', md: '275px' }}
-          w={'full'}
-          borderWidth="1px"
-          borderRadius="lg"
-          overflow="hidden"
-          bg={useColorModeValue('gray.100', 'gray.800')}
-          p={5}>
-          <Stack align={'start'} spacing={5}>
-            <Flex
-              w={16}
-              h={16}
-              align={'center'}
-              justify={'center'}
-              color={'white'}
-              rounded={'full'}
-              bg={useColorModeValue('gray.100', 'gray.700')}>
-              {icon}
-            </Flex>
-            <Box mt={2}>
-              <Heading color={'yellow.100'} size="md">{heading}</Heading>
-              <Text mt={1} fontSize={'sm'}>
-                {description}
-              </Text>
-            </Box>
-            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-                {link}
-            </Button>
-          </Stack>
-        </ChakraBox>
-    );
-  };
-  
-  export default function Works() {
-    return (
-      <Box p={4}>
-        <Stack spacing={3} as={Container} maxW={'3xl'} textAlign={'center'}>
-          <Text
-            bgGradient='linear(to-r, blue.200, yellow.200, pink.200)'
-            bgClip='text'
-            fontSize='5xl'
-            fontWeight='extrabold'
-          >
-            / Works
-          </Text>
-          <Text color={'white.600'} fontSize={{ base: 'sm', sm: 'lg' }}>
-            These are some of the things I&apos;ve made and some things I&apos;m still working on.
-          </Text>
+
+const Card = ({ heading, description, icon, link }: CardProps) => {
+  return (
+      <ChakraBox
+        variants={item}
+        initial={{ opacity: 0, rotateX: -10, scale: 0 }}
+        whileInView={{ opacity: 1, rotateX: 0, scale: 1 }}
+        viewport={{ once: true }}
+
+        whileHover={{ scale: 1.1 }}
+        onHoverStart={e => {}}
+        onHoverEnd={e => {}}
+
+        maxW={{ base: 'full', md: '275px' }}
+        w={'full'}
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        bg={useColorModeValue('gray.100', 'gray.800')}
+        p={5}>
+        <Stack align={'start'} spacing={5}>
+          <Flex
+            w={16}
+            h={16}
+            align={'center'}
+            justify={'center'}
+            color={'white'}
+            rounded={'full'}
+            bg={useColorModeValue('gray.100', 'gray.700')}>
+            {icon}
+          </Flex>
+          <Box mt={2}>
+            <Heading color={'yellow.100'} size="md">{heading}</Heading>
+            <Text mt={1} fontSize={'sm'}>
+              {description}
+            </Text>
+          </Box>
+          <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              {link}
+          </Button>
         </Stack>
+      </ChakraBox>
+  );
+};
 
-        <ChakraBox variants={ container }>
-          <Container maxW={'5xl'} mt={12}>
+export default function Works() {
+  return (
+    <Box p={4}>
+      <Stack spacing={3} as={Container} maxW={'3xl'} textAlign={'center'}>
+        <Text
+          bgGradient='linear(to-r, blue.200, yellow.200, pink.200)'
+          bgClip='text'
+          fontSize='5xl'
+          fontWeight='extrabold'
+        >
+          / Works
+        </Text>
+        <Text color={'white.600'} fontSize={{ base: 'sm', sm: 'lg' }}>
+          These are some of the things I&apos;ve made and some things I&apos;m still working on.
+        </Text>
+      </Stack>
 
-            <Flex flexWrap="wrap" gridGap={6} justify="center">
-                <Card
-                  heading={'Webdev'}
-                  icon={<Icon as={MdOutlineWebAsset} w={10} h={10} />}
-                  description={'My webdev portfolio. It is very unresponsive, and looks good on wide horizontal screens'}
-                  link={<Link href={'https://uglysquid.github.io/'} isExternal>Check it out</Link>}
-                />
+      <ChakraBox variants={ container }>
+        <Container maxW={'5xl'} mt={12}>
 
+          <Flex flexWrap="wrap" gridGap={6} justify="center">
               <Card
-                heading={'Graphics'}
-                icon={<Icon as={FaFolder} w={10} h={10} />}
-                description={
-                  'Google Drive with my art.'
-                }
-                link={<Link href={'https://drive.google.com/drive/folders/1LG_l5ZZzm3dc-UrBCX8tX5EJit5aSBp3?usp=sharing'} isExternal>Check it out</Link>}
+                heading={'Webdev'}
+                icon={<Icon as={MdOutlineWebAsset} w={10} h={10} />}
+                description={'My webdev portfolio. It is very unresponsive, and looks good on wide horizontal screens'}
+                link={<Link href={'https://uglysquid.github.io/'} isExternal>Check it out</Link>}
               />
 
-              <Card
-                heading={'Other Dev'}
-                icon={<Icon as={BsGithub} w={10} h={10} />}
-                description={
-                  'Github with my projects. '
-                }
-                link={<Link href={'https://github.com/UglySquid'} isExternal>Check it out</Link>}
-              />
-            </Flex>
-          </Container>
-        </ChakraBox>
-        
-      </Box>
-    );
-  }
-  
+            <Card
+              heading={'Graphics'}
+              icon={<Icon as={FaFolder} w={10} h={10} />}
+              description={
+                'Google Drive with my art.'
+              }
+              link={<Link href={'https://drive.google.com/drive/folders/1LG_l5ZZzm3dc-UrBCX8tX5EJit5aSBp3?usp=sharing'} isExternal>Check it out</Link>}
+            />
+
+            <Card
+              heading={'Other Dev'}
+              icon={<Icon as={BsGithub} w={10} h={10} />}
+              description={
+                'Github with my projects. '
+              }
+              link={<Link href={'https://github.com/UglySquid'} isExternal>Check it out</Link>}
+            />
+          </Flex>
+        </Container>
+      </ChakraBox>
+      
+    </Box>
+  );
+}
