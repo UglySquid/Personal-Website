6b8616cd637b95c5e51673269355f4da6df83672
UglySquid <christine.wei.63@gmail.com>
2023-06-14 09:54:08 -0400

    :D

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,16 @@
 import {
-    Container, SimpleGrid, Flex, Stack, Box,
-    Img, Button, Circle, Show,
-    Text, Heading,
-    extendTheme, ChakraProvider, chakra, shouldForwardProp, useColorModeValue, useColorMode, 
-    Modal, useDisclosure, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Icon,
+  Container, SimpleGrid, Flex, Stack, Box,
+  Img, Button, Circle, Show,
+  Text, Heading,
+  extendTheme, ChakraProvider, chakra, shouldForwardProp, useColorModeValue, useColorMode,
+  Modal, useDisclosure, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Icon,
 } from '@chakra-ui/react';
 
 import React, { ReactElement, Suspense } from 'react';
 import { isValidMotionProp, motion } from 'framer-motion';
 
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
-    
+
 interface FeatureProps {
   text: string;
   iconBg: string;
@@ -22,7 +22,7 @@ const breakpoints = {
   md: '48em', // 768px
   lg: '62em', // 992px
   xl: '80em', // 1280px
-'2xl': '96em', // 1536px
+  '2xl': '96em', // 1536px
 }
 
 const Font = extendTheme({
@@ -43,18 +43,18 @@ export default function Hero() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Container maxW={'5xl'} py={{base: 12, sm: 4}} centerContent>
+    <Container maxW={'5xl'} py={{ base: 12, sm: 4 }} centerContent>
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} alignItems={'center'}>
 
         <Show below='sm'>
-          <Box p='10' rounded='lg' bg={useColorModeValue('gray.100', 'gray.800')} height="300px">    
+          <Box p='10' rounded='lg' bg={useColorModeValue('gray.100', 'gray.800')} height="300px">
             <Suspense fallback={<div>Loading...Loading...Still Loading</div>}>
               <Spline scene="https://prod.spline.design/kvl1zb3sgfZKPr9Q/scene.splinecode" />
             </Suspense>
           </Box>
         </Show>
 
-        <Stack spacing={4} mb={{base: 100, sm: 0}}>
+        <Stack spacing={4} mb={{ base: 100, sm: 0 }}>
           <ChakraProvider theme={Font}>
             <Heading
               bgGradient='linear(to-r, blue.200, yellow.200, pink.200)'
@@ -63,16 +63,21 @@ export default function Hero() {
               fontWeight='extrabold'
             >
               Hello There!
-              
+
             </Heading>
           </ChakraProvider>
           <Text color={useColorModeValue('gray.800', 'white.500')} fontSize={'lg'}>
-          I&apos;m Christine, a grade 10 student at Milliken Mills High School.
-          An area I would like to explore is rotics and autonomous machines for space exploration. 
-          Some other things I&apos;m interested in are computer science, mathematics, astrophysics, quantum mechanics, and aerospace engineering. 
+            I&apos;m Christine, a 10th grade student at Milliken Mills High School.
+            I'm super into robotics and autonomous machines, especially for space exploration.
+            <br></br>
+            <br></br>
+            Right now, I'm diving into Python databases like SQL, and my future goal is to use Python for AI.
+            <br></br>
+            <br></br>
+            I can't wait to make a meaningful impact in the field and contribute to the advancement of space exploration with my passion for robotics and AI.
           </Text>
 
-          {/* <Button onClick={onOpen}>About Metallic Masquerade</Button>
+          {/* <Button onClick={onOpen}>About Metallic Masquerade</ >
             <Modal isOpen={isOpen} onClose={onClose}>
               <ModalOverlay />
               <ModalContent>
@@ -112,18 +117,18 @@ export default function Hero() {
                 </ModalFooter>
               </ModalContent>
             </Modal> */}
-          
+
         </Stack>
 
         <Show above='sm'>
-          <Box p='10' rounded='lg' bg={useColorModeValue('gray.100', 'gray.800')} height="500px">    
+          <Box p='10' rounded='lg' bg={useColorModeValue('gray.100', 'gray.800')} height="500px">
             <Suspense fallback={<div>Loading...</div>}>
-            <Spline scene="https://prod.spline.design/e-H2UFLaP5b5SHMa/scene.splinecode" />
+              <Spline scene="https://prod.spline.design/e-H2UFLaP5b5SHMa/scene.splinecode" />
             </Suspense>
           </Box>
         </Show>
-        
-          
+
+
       </SimpleGrid>
     </Container>
   );
