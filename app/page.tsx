8e3726584f22793a3cc5bcb81a10493edb052b9a
UglySquid <christine.wei.63@gmail.com>
2023-06-16 23:47:26 -0400

    sob troubleshooting

--- app/page.tsx
+++ app/page.tsx
@@ -28,86 +28,87 @@ const MotionBox = motion(forwardRef(({ layoutId, ...rest }, ref) => <chakra.div 
 
 export default function RootLayout() {
   return (
-    <ChakraProvider theme={theme}>
-      <html lang="en">
-        <head>
-          <title>Christine Wei :&#93;</title>
-          <link rel="icon" type="image/x-icon" href="astrobunny.jpg"/>
-        </head>
-      <body>
-        <Flex height="100%" alignItems="center" justifyContent="center">
-          <Flex direction="column" background="grey.100" p={{base: 13, sm: 4}} gap={{base: 0, sm: 100}}>
+    
+    // <html lang="en">
+    //   <head>
+    //     <title>Christine Wei :&#93;</title>
+    //     <link rel="icon" type="image/x-icon" href="astrobunny.jpg"/>
+    //   </head>
+    //   <body>
+        <ChakraProvider theme={theme}>
+          <Flex height="100%" alignItems="center" justifyContent="center">
+            <Flex direction="column" background="grey.100" p={{base: 13, sm: 4}} gap={{base: 0, sm: 100}}>
 
-              <MotionBox
-                initial={{ scaleX: 0 }}
-                whileInView={{ scaleX: 1 }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <Nav />
-              </MotionBox>
-              
+                <MotionBox
+                  initial={{ scaleX: 0 }}
+                  whileInView={{ scaleX: 1 }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <Nav />
+                </MotionBox>
+                
 
-              <MotionBox
-                initial={{ opacity: 0, scale: 0.5, y: -100 }}
-                whileInView={{ opacity: 1, scale: 1, y: 0  }}
-                viewport={{ once: true }}
-              >
-                <Hero />
-              </MotionBox>
+                <MotionBox
+                  initial={{ opacity: 0, scale: 0.5, y: -100 }}
+                  whileInView={{ opacity: 1, scale: 1, y: 0  }}
+                  viewport={{ once: true }}
+                >
+                  <Hero />
+                </MotionBox>
 
-              <MotionBox
-                initial={{ scaleX: 0 }}
-                whileInView={{ scaleX: 1  }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
-              </MotionBox>
-              
-              <MotionBox shouldWrapChildren
-                initial={{ opacity: 0, y: 200 }}
-                whileInView={{ opacity: 1, y: 0  }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <About />
-              </MotionBox>
+                <MotionBox
+                  initial={{ scaleX: 0 }}
+                  whileInView={{ scaleX: 1  }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
+                </MotionBox>
+                
+                <MotionBox shouldWrapChildren
+                  initial={{ opacity: 0, y: 200 }}
+                  whileInView={{ opacity: 1, y: 0  }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <About />
+                </MotionBox>
 
-              
+                
 
-              <MotionBox
-                initial={{ scaleX: 0 }}
-                whileInView={{ scaleX: 1  }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
-              </MotionBox>
-              
+                <MotionBox
+                  initial={{ scaleX: 0 }}
+                  whileInView={{ scaleX: 1  }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
+                </MotionBox>
+                
 
-              <MotionBox
-                initial={{ opacity: 0, y: 200 }}
-                whileInView={{ opacity: 1, y: 0  }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <Works />
-              </MotionBox>
-              
-              <MotionBox
-                initial={{ scaleX: 0 }}
-                whileInView={{ scaleX: 1  }}
-                transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                viewport={{ once: true }}
-              >
-                <Footer />
-              </MotionBox>
-              
+                <MotionBox
+                  initial={{ opacity: 0, y: 200 }}
+                  whileInView={{ opacity: 1, y: 0  }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <Works />
+                </MotionBox>
+                
+                <MotionBox
+                  initial={{ scaleX: 0 }}
+                  whileInView={{ scaleX: 1  }}
+                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
+                  viewport={{ once: true }}
+                >
+                  <Footer />
+                </MotionBox>
+                
+            </Flex>
           </Flex>
-        </Flex>
-      </body>
-    </html>
-  </ChakraProvider>
+        </ChakraProvider>
+    //   </body>
+    // </html>
   )
 }
