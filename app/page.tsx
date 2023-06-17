b37e504cc426a833f82891f3a12c6d55fe2eceb9
UglySquid <christine.wei.63@gmail.com>
2023-06-16 23:53:37 -0400

    maybe without the motion it will work?

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,141 @@
 "use client"
 
+// import {
+//   Flex,
+//   ChakraProvider, chakra, shouldForwardProp, forwardRef,
+//   Skeleton,
+//   extendTheme,
+// } from '@chakra-ui/react'
+
+
+// import { motion } from 'framer-motion';
+// import React, { useRef, useEffect } from 'react';
+  
+// import Hero from '@/components/hero'
+// import Works from '@/components/works'
+// import About from '@/components/about'
+// import Nav from '@/components/nav'
+// import Footer from '@/components/footer'
+
+// const theme = extendTheme({
+//   config: {
+//     initialColorMode: "dark",
+//     useSystemColorMode: false,
+//   }
+// });
+
+// const MotionBox = motion(forwardRef(({ layoutId, ...rest }, ref) => <chakra.div ref={ref} {...rest} />))
+
+// export default function RootLayout() {
+//   return (
+    
+//     <html lang="en">
+//       <head>
+//         <title>Christine Wei :&#93;</title>
+//         <link rel="icon" type="image/x-icon" href="astrobunny.jpg"/>
+//       </head>
+//       <body>
+//         <ChakraProvider theme={theme}>
+//           <Flex height="100%" alignItems="center" justifyContent="center">
+//             <Flex direction="column" background="grey.100" p={{base: 13, sm: 4}} gap={{base: 0, sm: 100}}>
+
+//                 <MotionBox
+//                   initial={{ scaleX: 0 }}
+//                   whileInView={{ scaleX: 1 }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Nav />
+//                 </MotionBox>
+                
+
+//                 <MotionBox
+//                   initial={{ opacity: 0, scale: 0.5, y: -100 }}
+//                   whileInView={{ opacity: 1, scale: 1, y: 0  }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Hero />
+//                 </MotionBox>
+
+//                 <MotionBox
+//                   initial={{ scaleX: 0 }}
+//                   whileInView={{ scaleX: 1  }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
+//                 </MotionBox>
+                
+//                 <MotionBox shouldWrapChildren
+//                   initial={{ opacity: 0, y: 200 }}
+//                   whileInView={{ opacity: 1, y: 0  }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <About />
+//                 </MotionBox>
+
+                
+
+//                 <MotionBox
+//                   initial={{ scaleX: 0 }}
+//                   whileInView={{ scaleX: 1  }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
+//                 </MotionBox>
+                
+
+//                 <MotionBox
+//                   initial={{ opacity: 0, y: 200 }}
+//                   whileInView={{ opacity: 1, y: 0  }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Works />
+//                 </MotionBox>
+                
+//                 <MotionBox
+//                   initial={{ scaleX: 0 }}
+//                   whileInView={{ scaleX: 1  }}
+//                   transition={{ type: "spring", stiffness: 100, duration: 2 }}
+//                   viewport={{ once: true }}
+//                 >
+//                   <Footer />
+//                 </MotionBox>
+                
+//             </Flex>
+//           </Flex>
+//         </ChakraProvider>
+//       </body>
+//     </html>
+//   )
+// }
+
 import {
   Flex,
-  ChakraProvider, chakra, shouldForwardProp, forwardRef,
+  ChakraProvider,
+  chakra,
   Skeleton,
   extendTheme,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 
-
-import { motion } from 'framer-motion';
-import React, { useRef, useEffect } from 'react';
-  
-import Hero from '@/components/hero'
-import Works from '@/components/works'
-import About from '@/components/about'
-import Nav from '@/components/nav'
-import Footer from '@/components/footer'
+import Hero from '@/components/hero';
+import Works from '@/components/works';
+import About from '@/components/about';
+import Nav from '@/components/nav';
+import Footer from '@/components/footer';
 
 const theme = extendTheme({
   config: {
     initialColorMode: "dark",
     useSystemColorMode: false,
-  }
+  },
 });
-
-const MotionBox = motion(forwardRef(({ layoutId, ...rest }, ref) => <chakra.div ref={ref} {...rest} />))
 
 export default function RootLayout() {
   return (
-    
     <html lang="en">
       <head>
         <title>Christine Wei :&#93;</title>
@@ -39,76 +146,25 @@ export default function RootLayout() {
           <Flex height="100%" alignItems="center" justifyContent="center">
             <Flex direction="column" background="grey.100" p={{base: 13, sm: 4}} gap={{base: 0, sm: 100}}>
 
-                <MotionBox
-                  initial={{ scaleX: 0 }}
-                  whileInView={{ scaleX: 1 }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <Nav />
-                </MotionBox>
-                
+              <Nav />
 
-                <MotionBox
-                  initial={{ opacity: 0, scale: 0.5, y: -100 }}
-                  whileInView={{ opacity: 1, scale: 1, y: 0  }}
-                  viewport={{ once: true }}
-                >
-                  <Hero />
-                </MotionBox>
+              <Hero />
 
-                <MotionBox
-                  initial={{ scaleX: 0 }}
-                  whileInView={{ scaleX: 1  }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
-                </MotionBox>
-                
-                <MotionBox shouldWrapChildren
-                  initial={{ opacity: 0, y: 200 }}
-                  whileInView={{ opacity: 1, y: 0  }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <About />
-                </MotionBox>
+              <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
 
-                
+              <About />
 
-                <MotionBox
-                  initial={{ scaleX: 0 }}
-                  whileInView={{ scaleX: 1  }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
-                </MotionBox>
-                
+              <Skeleton startColor='yellow.200' endColor='pink.200' height='10px' />
 
-                <MotionBox
-                  initial={{ opacity: 0, y: 200 }}
-                  whileInView={{ opacity: 1, y: 0  }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <Works />
-                </MotionBox>
-                
-                <MotionBox
-                  initial={{ scaleX: 0 }}
-                  whileInView={{ scaleX: 1  }}
-                  transition={{ type: "spring", stiffness: 100, duration: 2 }}
-                  viewport={{ once: true }}
-                >
-                  <Footer />
-                </MotionBox>
-                
+              <Works />
+
+              <Footer />
+
             </Flex>
           </Flex>
         </ChakraProvider>
       </body>
     </html>
-  )
+  );
 }
+
